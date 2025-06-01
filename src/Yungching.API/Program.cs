fef545685f41a50789d479b1220f7bfe41b50c8b
kevincheng <kevincheng401@gmail.com>
2025-06-01 18:45:43 +0800

    Remove additional comments.

--- src/Yungching.API/Program.cs
+++ src/Yungching.API/Program.cs
@@ -3,14 +3,6 @@ using Yungching.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// builder.Services.AddSingleton<IDbConnectionFactory>(sp =>
-// {
-//     var configuration = sp.GetRequiredService<IConfiguration>();
-//     var connectionString = configuration.GetConnectionString("DefaultConnection");
-//     return new SqlConnectionFactory(connectionString!);
-// });
-// builder.Services.AddTransient<IOrderRepository, OrderRepository>();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
